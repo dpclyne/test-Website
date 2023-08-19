3212d155b5820b399777c484c1f4d5fc9d61eea3
dpclyne <142657833+dpclyne@users.noreply.github.com>
2023-08-19 16:48:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-// Get all listing cards
-const listingCards = document.querySelectorAll('.listing-card');
+document.addEventListener("DOMContentLoaded", function () {
+    // Fetch the JSON data (simulated as a local file)
+    fetch("listings.json")
+        .then(response => response.json())
+        .then(data => {
+            // Generate listing cards based on the JSON data
+            const listingsContainer = document.querySelector(".listings");
 
-// Add click event listeners to each card
-listingCards.forEach(card => {
-    card.addEventListener('click', () => {
-        // Simulate loading the individual listing HTML page
-        window.location.href = 'listing1.html'; // Replace with the actual URL
-    });
+            data.forEach(listing => {
+                const card = document.createElement("div");
+                card.classList.add("listing-card");
+                card.innerHTML = `
+                    <img src="${listing.thumbnail}" alt="${listing.title}">
+                    <h2>${listing.title}</h2>
+                    <p class="location">${listing.location}</p>
+                    <p class="price">${listing.price}</p>
+                `;
+
+                // Add a click event listener to simulate loading individual listing pages
+                card.addEventListener("click", () => {
+                    // Simulate loading the individual listing HTML page
+                    window.location.href = `listing${listing.id}.html`;
+                });
+
+                listingsContainer.appendChild(card);
+            });
+        })
+        .catch(error => {
+            console.error("Error loading JSON data:", error);
+        });
 });
